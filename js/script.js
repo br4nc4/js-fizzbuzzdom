fe524425a6eba3d473c49a2105f6ad63344a5a41
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-21 23:27:17 +0200

    milestone2 pt.5 working

--- js/script.js
+++ js/script.js
@@ -1,13 +1,110 @@
 "use strict"
 
 /* MILESTONE 2 */
-const numberElement = document.getElementById("number");
-const fizzElement = document.getElementById("numberFizz");
-const buzzElement = document.getElementById("numberBuzz");
-const fizzBuzzElement = document.getElementById("numberFizzBuzz");
+const numberElement1 = document.getElementById("number1");
+const numberElement2 = document.getElementById("number2");
+const numberElement3 = document.getElementById("number3");
+const numberElement4 = document.getElementById("number4");
+const numberElement5 = document.getElementById("number5");
+const numberElement6 = document.getElementById("number6");
+const numberElement7 = document.getElementById("number7");
+const numberElement8 = document.getElementById("number8");
+const numberElement9 = document.getElementById("number9");
+const numberElement10 = document.getElementById("number10");
+const numberElement11 = document.getElementById("number11");
+const numberElement12 = document.getElementById("number12");
+const numberElement13 = document.getElementById("number13");
+const numberElement14 = document.getElementById("number14");
+const numberElement15 = document.getElementById("number15");
+const numberElement16 = document.getElementById("number16");
+const numberElement17 = document.getElementById("number17");
+const numberElement18 = document.getElementById("number18");
+const numberElement19 = document.getElementById("number19");
+const numberElement20 = document.getElementById("number20");
+const numberElement21 = document.getElementById("number21");
+const numberElement22 = document.getElementById("number22");
+const numberElement23 = document.getElementById("number23");
+const numberElement24 = document.getElementById("number24");
+const numberElement25 = document.getElementById("number25");
+const numberElement26 = document.getElementById("number26");
+const numberElement27 = document.getElementById("number27");
+const numberElement28 = document.getElementById("number28");
+const numberElement29 = document.getElementById("number29");
+const numberElement30 = document.getElementById("number30");
+const numberElement31 = document.getElementById("number31");
+const numberElement32 = document.getElementById("number32");
+const numberElement33 = document.getElementById("number33");
+const numberElement34 = document.getElementById("number34");
+const numberElement35 = document.getElementById("number35");
+const numberElement36 = document.getElementById("number36");
+const numberElement37 = document.getElementById("number37");
+const numberElement38 = document.getElementById("number38");
+const numberElement39 = document.getElementById("number39");
+const numberElement40 = document.getElementById("number40");
+const numberElement41 = document.getElementById("number41");
+const numberElement42 = document.getElementById("number42");
+const numberElement43 = document.getElementById("number43");
+const numberElement44 = document.getElementById("number44");
+const numberElement45 = document.getElementById("number45");
+const numberElement46 = document.getElementById("number46");
+const numberElement47 = document.getElementById("number47");
+const numberElement48 = document.getElementById("number48");
+const numberElement49 = document.getElementById("number49");
+const numberElement50 = document.getElementById("number50");
+const numberElement51 = document.getElementById("number51");
+const numberElement52 = document.getElementById("number52");
+const numberElement53 = document.getElementById("number53");
+const numberElement54 = document.getElementById("number54");
+const numberElement55 = document.getElementById("number55");
+const numberElement56 = document.getElementById("number56");
+const numberElement57 = document.getElementById("number57");
+const numberElement58 = document.getElementById("number58");
+const numberElement59 = document.getElementById("number59");
+const numberElement60 = document.getElementById("number60");
+const numberElement61 = document.getElementById("number61");
+const numberElement62 = document.getElementById("number62");
+const numberElement63 = document.getElementById("number63");
+const numberElement64 = document.getElementById("number64");
+const numberElement65 = document.getElementById("number65");
+const numberElement66 = document.getElementById("number66");
+const numberElement67 = document.getElementById("number67");
+const numberElement68 = document.getElementById("number68");
+const numberElement69 = document.getElementById("number69");
+const numberElement70 = document.getElementById("number70");
+const numberElement71 = document.getElementById("number71");
+const numberElement72 = document.getElementById("number72");
+const numberElement73 = document.getElementById("number73");
+const numberElement74 = document.getElementById("number74");
+const numberElement75 = document.getElementById("number75");
+const numberElement76 = document.getElementById("number76");
+const numberElement77 = document.getElementById("number77");
+const numberElement78 = document.getElementById("number78");
+const numberElement79 = document.getElementById("number79");
+const numberElement80 = document.getElementById("number80");
+const numberElement81 = document.getElementById("number81");
+const numberElement82 = document.getElementById("number82");
+const numberElement83 = document.getElementById("number83");
+const numberElement84 = document.getElementById("number84");
+const numberElement85 = document.getElementById("number85");
+const numberElement86 = document.getElementById("number86");
+const numberElement87 = document.getElementById("number87");
+const numberElement88 = document.getElementById("number88");
+const numberElement89 = document.getElementById("number89");
+const numberElement90 = document.getElementById("number90");
+const numberElement91 = document.getElementById("number91");
+const numberElement92 = document.getElementById("number92");
+const numberElement93 = document.getElementById("number93");
+const numberElement94 = document.getElementById("number94");
+const numberElement95 = document.getElementById("number95");
+const numberElement96 = document.getElementById("number96");
+const numberElement97 = document.getElementById("number97");
+const numberElement98 = document.getElementById("number98");
+const numberElement99 = document.getElementById("number99");
+const numberElement100 = document.getElementById("number100");
+
 
 /* TEST PER VERIFICARE CHE SIANO EFFETTIVAMENTE PRESE DALL'HTML */
-console.log(numberElement, fizzElement, buzzElement, fizzBuzzElement);
+
 
 
 /* MILESTONE 1 */
@@ -15,19 +112,126 @@ console.log(numberElement, fizzElement, buzzElement, fizzBuzzElement);
 for (let i=1; i<=100; i++){
     if (i % 3 === 0 && i % 5 === 0){
         console.log("FizzBuzz");
-        fizzBuzzElement.innerHTML += "FizzBuzz";
+        /* MILESTONE 2 */
+        numberElement15.innerHTML = "FizzBuzz";
+        numberElement30.innerHTML = "FizzBuzz";
+        numberElement45.innerHTML = "FizzBuzz";
+        numberElement60.innerHTML = "FizzBuzz";
+        numberElement75.innerHTML = "FizzBuzz";
+        numberElement90.innerHTML = "FizzBuzz";
     } else if (i % 3 === 0) {
         console.log("Fizz");
-        fizzElement.innerHTML += "Fizz";
+        /* MILESTONE 2 */
+        numberElement3.innerHTML = "Fizz";
+        numberElement6.innerHTML = "Fizz";
+        numberElement9.innerHTML = "Fizz";
+        numberElement12.innerHTML = "Fizz";
+        numberElement18.innerHTML = "Fizz";
+        numberElement21.innerHTML = "Fizz";
+        numberElement24.innerHTML = "Fizz";
+        numberElement27.innerHTML = "Fizz";
+        numberElement33.innerHTML = "Fizz";
+        numberElement48.innerHTML = "Fizz";
+        numberElement51.innerHTML = "Fizz";
+        numberElement54.innerHTML = "Fizz";
+        numberElement57.innerHTML = "Fizz";
+        numberElement63.innerHTML = "Fizz";
+        numberElement66.innerHTML = "Fizz";
+        numberElement69.innerHTML = "Fizz";
+        numberElement72.innerHTML = "Fizz";
+        numberElement78.innerHTML = "Fizz";
+        numberElement81.innerHTML = "Fizz";
+        numberElement84.innerHTML = "Fizz";
+        numberElement93.innerHTML = "Fizz";
+        numberElement96.innerHTML = "Fizz";
+        numberElement99.innerHTML = "Fizz";
     } else if (i % 5 === 0){
         console.log("Buzz");
-        buzzElement.innerHTML += "Buzz";
+        /* MILESTONE 2 */
+        numberElement5.innerHTML = "Buzz";
+        numberElement10.innerHTML = "Buzz";
+        numberElement20.innerHTML = "Buzz";
+        numberElement25.innerHTML = "Buzz";
+        numberElement35.innerHTML = "Buzz";
+        numberElement40.innerHTML = "Buzz";
+        numberElement50.innerHTML = "Buzz";
+        numberElement55.innerHTML = "Buzz";
+        numberElement65.innerHTML = "Buzz";
+        numberElement70.innerHTML = "Buzz";
+        numberElement80.innerHTML = "Buzz";
+        numberElement85.innerHTML = "Buzz";
+        numberElement95.innerHTML = "Buzz";
+        numberElement100.innerHTML = "Buzz";
     } else {
-        console.log(i)
-        numberElement.innerHTML += i;
+        console.log(i);
+        /* MILESTONE 2 */
+        numberElement1.innerHTML = "1";
+        numberElement2.innerHTML = "2";
+        numberElement4.innerHTML = "4";
+        numberElement7.innerHTML = "7";
+        numberElement8.innerHTML = "8";
+        numberElement11.innerHTML = "11";
+        numberElement13.innerHTML = "13";
+        numberElement14.innerHTML = "14";
+        numberElement16.innerHTML = "16";
+        numberElement17.innerHTML = "17";
+        numberElement19.innerHTML = "19";
+        numberElement22.innerHTML = "22";
+        numberElement23.innerHTML = "23";
+        numberElement26.innerHTML = "26";
+        numberElement28.innerHTML = "28";
+        numberElement29.innerHTML = "29";
+        numberElement31.innerHTML = "31";
+        numberElement32.innerHTML = "32";
+        numberElement34.innerHTML = "34";
+        numberElement37.innerHTML = "37";
+        numberElement38.innerHTML = "38";
+        numberElement41.innerHTML = "41";
+        numberElement43.innerHTML = "43";
+        numberElement44.innerHTML = "44";
+        numberElement46.innerHTML = "46";
+        numberElement47.innerHTML = "47";
+        numberElement49.innerHTML = "49";
+        numberElement52.innerHTML = "52";
+        numberElement53.innerHTML = "53";
+        numberElement56.innerHTML = "56";
+        numberElement58.innerHTML = "58";
+        numberElement59.innerHTML = "59";
+        numberElement61.innerHTML = "61";
+        numberElement62.innerHTML = "62";
+        numberElement64.innerHTML = "64";
+        numberElement67.innerHTML = "67";
+        numberElement68.innerHTML = "68";
+        numberElement71.innerHTML = "71";
+        numberElement73.innerHTML = "73";
+        numberElement76.innerHTML = "76";
+        numberElement77.innerHTML = "77";
+        numberElement79.innerHTML = "79";
+        numberElement82.innerHTML = "82";
+        numberElement83.innerHTML = "83";
+        numberElement86.innerHTML = "86";
+        numberElement88.innerHTML = "88";
+        numberElement89.innerHTML = "89";
+        numberElement91.innerHTML = "91";
+        numberElement92.innerHTML = "92";
+        numberElement94.innerHTML = "94";
+        numberElement97.innerHTML = "97";
+        numberElement98.innerHTML = "98";
     }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
